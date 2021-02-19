3ee46a0c5d9cec85546af9adecdfdfe710354382
Martin Vala <mavala@redhat.com>
2021-02-19 15:09:10 +0100

    Fix
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- action.sh
+++ action.sh
@@ -5,5 +5,5 @@ echo "OPA_NAME=$OPA_NAME"
 echo "OPA_VERSION=$OPA_VERSION"
 echo "OPA_OPERATOR_DIR=$OPA_OPERATOR_DIR"
 
-
+git clone https://github.com/operator-framework/community-operators.git
 ls -al 
